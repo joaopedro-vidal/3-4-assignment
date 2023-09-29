386d89bf53f30d319e418ac65f32531f72151491
mark-elias <mark7elias@gmail.com>
2023-09-29 16:02:42 -0700

    basic Functions created

--- calculator.js
+++ calculator.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+
+function add(a, b){
+    console.log("add function called");
+    return a + b;
+}
+function subtract(a, b){
+    console.log("subtract function called");
+    return a - b;
+}
+function multiply(a, b){
+    console.log("multiply function called");
+    return a * b;
+}
+function divide(a, b){
+    console.log("divide function called");
+    return a / b;
+}
