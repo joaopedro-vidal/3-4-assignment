002f08bd423f483b0f0ddd67482320d7ff6719b6
victorsanchez030496 <victorsanchezmendoza34@gmail.com>
2023-09-29 23:24:21 -0600

    Changes
    Add table for calculator, css and js

--- calculator.js
+++ calculator.js
@@ -16,3 +16,22 @@ function divide(a, b){
     console.log("divide function called");
     return a / b;
 }
+
+function setResult(value) {
+    document.getElementById('result').innerHTML = value;
+}
+function getResult() {
+    return(document.getElementById('result').innerHTML);
+}
+function add(key) {
+    var result = getResult();
+    if (result!='0' || isNaN(key)) setResult(result + key);
+    else setResult(key);
+}
+function calc() {
+    var result = eval(getResult());
+    setResult(result);
+}
+function del() {
+    setResult(0);
+}
