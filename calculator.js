f1a9669c12f41d256d0ceeab1e9d4e1b520c20ac
victorsanchez030496 <victorsanchezmendoza34@gmail.com>
2023-10-05 00:52:35 -0600

    Funtionality calculator

--- calculator.js
+++ calculator.js
@@ -1,66 +1,48 @@
-let displayNumber = '0';
-let number1 = 0;
-let number2 = 0;
-let operator = ''
+var displayNumber = '';
+var result = 0;
+var operator = 0;
 
-/*function add(a, b){
-    console.log("add function called");
-    return a + b;
-}
-function subtract(a, b){
-    console.log("subtract function called");
-    return a - b;
-}
-function multiply(a, b){
-    console.log("multiply function called");
-    return a * b;
-}
-function divide(a, b){
-    console.log("divide function called");
-    return a / b;
-}
 
-function setResult(value) {
-    document.getElementById('result').innerHTML = value;
-}
-function getResult() {
-    return(document.getElementById('result').innerHTML);
-}
-function add(key) {
-    var result = getResult();
-    if (result!='0' || isNaN(key)) setResult(result + key);
-    else setResult(key);
-}
-function calc() {
-    var result = eval(getResult());
-    setResult(result);
-}
-function del() {
-    setResult(0);
-}
-*/
-/*************************************** */
 function updateDisplay(newNumber) {
-    if (displayNumber === '0' && newNumber !== '.')
-        displayNumber = newNumber.toString();
-    else
-    {
-        if (newNumber === '.' && displayNumber.includes('.'))
-            return
-        displayNumber = displayNumber + newNumber;
+    displayNumber = document.getElementById('numbers-display').value;
+    if (displayNumber != '' || isNaN(newNumber)) {
+        document.getElementById('numbers-display').value = displayNumber + newNumber;
+    } else {
+        document.getElementById('numbers-display').value = newNumber;
     }
+}
+function clearDisplay() {
+    displayNumber = "";
+    operator = 0;
+    result = 0;
     document.getElementById('numbers-display').value = displayNumber;
 }
-function clearDisplay(){
-    displayNumber="";
-    document.getElementById('numbers-display').value = displayNumber;
+function saveNumber(simble) {
+    displayNumber = document.getElementById('numbers-display').value;
+    if (operator == 0 && displayNumber != "") {
+        document.getElementById('numbers-display').value = displayNumber + simble;
+    } else if (operator == 0 && displayNumber == "" && simble == "-") {
+        document.getElementById('numbers-display').value = simble;
+    } else {
+        operate();
+        displayNumber = document.getElementById('numbers-display').value;
+        document.getElementById('numbers-display').value = displayNumber + simble;
+    }
+    operator++;
 }
-function undo(){
-
+function operate() {
+    displayNumber = document.getElementById('numbers-display').value;
+    result = eval(displayNumber);
+    document.getElementById('numbers-display').value = result;
+    operator = 0;
 }
-function saveNumber(operator){
 
-}
-function operate(){
 
+function undo() {
+    displayNumber = document.getElementById('numbers-display').value;
+    let value=displayNumber.charAt(displayNumber.length - 1); // get the last char
+    if(value=="+" || value=="-" || value=="/" || value=="*"  ){
+        operator = 0;
+    }
+    document.getElementById('numbers-display').value = displayNumber.slice(0, -1); // Remove the last character
 }
