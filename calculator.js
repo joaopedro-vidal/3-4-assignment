a599fcf485d066dde0af2f8403bca735cea2d462
GuitounYoucef <68856205+GuitounYoucef@users.noreply.github.com>
2023-10-08 15:33:32 +0100

    second display fixe

--- calculator.js
+++ calculator.js
@@ -23,51 +23,58 @@ function division(num1, num2) {
 
 /************************************************************************************* */
 function updateDisplay(simble) {
-    if (simble === '.' && mainDisplay.includes('.'))    
+    if (simble === '.' && mainDisplay.includes('.'))
         return;
     if (mainDisplay === '0')
-          mainDisplay = simble.toString();
+        mainDisplay = simble.toString();
     else
-        mainDisplay = mainDisplay + simble;   
-     document.getElementById('main-numbers-display').value = mainDisplay;
+        mainDisplay = mainDisplay + simble;
+    document.getElementById('main-numbers-display').value = mainDisplay;
 
 }
 
 
 function saveNumber(oper) {
-    updateSecondDislpay(mainDisplay,oper)
-    
+    updateSecondDislpay(mainDisplay, oper)
+
     number1 = Number(mainDisplay);
     mainDisplay = '0';
     operation = oper;
 }
 
-function updateSecondDislpay(number,oper)
-{
-    if(oper==='=')    
-    {
-        secondDisplay=secondDisplay+number+oper;
-        document.getElementById('second-numbers-display').innerHTML = secondDisplay;
-        return;
+function updateSecondDislpay(number, oper) {
+    if (secondDisplay.length > 1) {
+        if(secondDisplay[secondDisplay.length - 1] === '=')
+        {
+            secondDisplay = number.toString()+oper; 
+            document.getElementById('second-numbers-display').innerHTML = secondDisplay;           
+            return;
+        }
+
+        if (oper === '=') 
+        {
+            secondDisplay = secondDisplay + number + oper;
+            document.getElementById('second-numbers-display').innerHTML = secondDisplay;
+            return;
+        }
+
+        if (isNaN(secondDisplay[secondDisplay.length - 1])) {
+            secondDisplay = secondDisplay.slice(0, -1) + oper;
+            document.getElementById('second-numbers-display').innerHTML = secondDisplay;
+            return;
+        }
     }
-    if(secondDisplay.length>1)
-    if(isNaN(secondDisplay[secondDisplay.length-1]) && secondDisplay[secondDisplay.length-1]!=='=')
-    {
-      secondDisplay=secondDisplay.slice(0, -1) + oper;
-      document.getElementById('second-numbers-display').innerHTML = secondDisplay;
-      return;
-    }
-    secondDisplay=secondDisplay+number+oper;
+    secondDisplay = secondDisplay + number + oper;
     document.getElementById('second-numbers-display').innerHTML = secondDisplay;
-    console.log('operation : ',secondDisplay);
-    
+    console.log('operation : ', secondDisplay);
+
 }
 
 function calculate() {
-    updateSecondDislpay(mainDisplay,'=');
+    updateSecondDislpay(mainDisplay, '=');
     number2 = Number(mainDisplay);
     let result = operate(operation, number1, number2);
-    console.log('result : ',result);
+    console.log('result : ', result);
     mainDisplay = result.toString();
     document.getElementById('main-numbers-display').value = mainDisplay;
 }
@@ -91,6 +98,8 @@ function operate(operation, number1, number2) {
 function clearDisplay() {
     mainDisplay = "0";
     document.getElementById('main-numbers-display').value = mainDisplay;
+    secondDisplay = "";
+    document.getElementById('second-numbers-display').innerHTML = secondDisplay;
 }
 
 function undo() {
